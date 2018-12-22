93eba0a99fcbc64b3688a39fdee802ed76adfb39
Alexander Bronca <alexander.bronca@gmail.com>
2018-12-22 12:17:05 -0500

    Add sandbox

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,49 @@
 module Main exposing (main)
 
+import Browser exposing (sandbox)
 import Element exposing (..)
+import Html exposing (Html)
 
 
+main : Program () Model Msg
 main =
+    sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    List Bool
+
+
+init : Model
+init =
+    []
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     layout [] <| text "hello world"
